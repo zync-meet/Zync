c75cd67eef9e431466b7a8abd56b59a1c42a2c4a
prem22k <premsai224k@gmail.com>
2026-01-16 18:35:47 +0530

    perf: Skip double Chrome download to speed up deploys

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -2,11 +2,13 @@
 # exit on error
 set -o errexit
 
-# Puppeteer will automatically pick up the cache location from .puppeteerrc.cjs
-# which is set to ./.cache/puppeteer (inside the project)
-echo "Installing Puppeteer Browsers..."
-npx puppeteer browsers install chrome
+# Skip Chrome download during npm install (we install it manually below)
+export PUPPETEER_SKIP_DOWNLOAD=true
 
 echo "Installing Dependencies..."
 npm install
 npm run build
+
+# Now install Chrome using the config file (.puppeteerrc.cjs)
+echo "Installing Puppeteer Browsers..."
+npx puppeteer browsers install chrome
