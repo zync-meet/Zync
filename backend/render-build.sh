6ecb1d623a8bf066ecdeb9c7790f8662ccfc95b8
prem22k <premsai224k@gmail.com>
2026-01-16 18:17:24 +0530

    chore: Finalize render-build.sh comments

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+# Puppeteer will automatically pick up the cache location from .puppeteerrc.cjs
+# which is set to ./.cache/puppeteer (inside the project)
 echo "Installing Puppeteer Browsers..."
 npx puppeteer browsers install chrome
 
