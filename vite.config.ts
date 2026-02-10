d0fb8ddde61e5dc0e5927ddaf87439aacf6e41c4
Chitkul Lakshya <chitkullakshya@gmail.com>
2026-02-10 13:07:21 +0530

    fixed electron errors

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,16 @@ export default defineConfig(({ mode }) => ({
       'Content-Security-Policy': "script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: https://apis.google.com https://www.googleapis.com https://www.gstatic.com; frame-src 'self' https://*.firebaseapp.com https://*.google.com; connect-src 'self' http://localhost:* ws://localhost:* https://*.googleapis.com https://*.firebaseio.com https://*.ws.pusherapp.com https://api.github.com https://github.com https://*.onrender.com wss://*.onrender.com; object-src 'none';"
     }
   },
+  optimizeDeps: {
+    entries: ['index.html'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
